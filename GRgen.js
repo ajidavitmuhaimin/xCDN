342220c74830235c27912b9225de38ae87801529
ajidavitmuhaimin <103036177+ajidavitmuhaimin@users.noreply.github.com>
2022-04-06 17:52:47 +0700

    Delete GRgen.js

--- GRgen.js
+++
@@ -1,8 +0,0 @@
-console.log('Made by Aji Davit Muhaimin');
-console.log("Make at 08.29PM 10 March 2022");
-var object=document.getElementById("viewer");
-object.src="https://chart.apis.google.com/chart?cht=qr&chs=250x250&chl=Alhamdulillah";
-function generate(){
-  var text=prompt("Enter Text to Convert");
-  object.src="https://chart.apis.google.com/chart?cht=qr&chs=250x250&chl="+text;
-};
