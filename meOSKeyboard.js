e65e5dbad9aa3b3b2a1c52714b32b5ce8c8d684e
ajidavitmuhaimin <103036177+ajidavitmuhaimin@users.noreply.github.com>
2022-04-18 09:49:25 +0700

    meOSKeyboard.js

--- meOSKeyboard.js
+++ meOSKeyboard.js
@@ -1,7 +1,7 @@
 document.onkeydown=function(meos){
   switch(meos.keyCode){
     ///Backspace///
-    case 77:
+    case 32:
       var se=prompt("Enter program name.html");
         window.location.href=se;
       break;
