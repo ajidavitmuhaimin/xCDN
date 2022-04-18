52eae9355747a5d2afdb41baff4a39994b5b859d
ajidavitmuhaimin <103036177+ajidavitmuhaimin@users.noreply.github.com>
2022-04-18 13:39:06 +0700

    meOSSetting.html

---
+++ meOSSeting.js
@@ -0,0 +1,10 @@
+function account(){
+  window.location.href="account.html";
+}
+function about(){
+  alert("meOS v1");
+  alert("Developed by Aji Davit Muhaimin");
+}
+function off(){
+  window.location.href="off.html";
+}
