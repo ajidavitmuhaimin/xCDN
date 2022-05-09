b2b55c1dd8a930abd0dff5033c46fa9c0398e60d
ajidavitmuhaimin <103036177+ajidavitmuhaimin@users.noreply.github.com>
2022-05-09 20:53:54 +0700

    ftl.js
    WhatsApp Fancy Text Library

---
+++ ftl.js
@@ -0,0 +1,14 @@
+function get(){
+    var df=document.getElementById("in").value;
+    var res=document.getElementById("res");
+    var res2=document.getElementById("res2");
+    var res3=document.getElementById("res3");
+    var res4=document.getElementById("res4");
+    res.innerHTML="_"+df+"_";
+    res2.innerHTML="```"+df+"```";
+    res3.innerHTML="~"+df+"~";
+    res4.innerHTML="*"+df+"*";
+  };
+  function about(){
+    alert("Developed by Aji Davit Muhaimin");
+  };
