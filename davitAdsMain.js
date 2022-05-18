90a2554a188aa0c3e1c599aa3cfc093e2004939b
ajidavitmuhaimin <103036177+ajidavitmuhaimin@users.noreply.github.com>
2022-05-18 10:44:30 +0700

    davitAdsMain.js

--- davitAdsMain.js
+++ davitAdsMain.js
@@ -1,2 +1,2 @@
-var text=document.getElementById("text").innerHTML="Delta, save your note privatelly,not save to your storage, download now on Amazon AppStore";
+var text=document.getElementById("text").innerHTML="Delta, save your note privatelly,simple,and safe, download now on Amazon AppStore";
 var image=document.getElementById("image").src="https://drive.google.com/uc?export=view&id=1eSxwPaJkMCJ4EAY1llPTjp55ROpGsCCp";
