7a0dda0d287d6b59511cfb5fd0b8f2752f1d83c9
Aji Davit Muhaimin <103036177+ajidavitmuhaimin@users.noreply.github.com>
2023-04-12 18:49:06 +0700

    fontart.js
    Javascript library for load Google Fonts

---
+++ fontart.js
@@ -0,0 +1,11 @@
+class FontArt{
+  static loadFont(documentId,fontName,fontNameCSS){
+    var createLink=document.createElement("link");
+    createLink.setAttribute("rel","stylesheet");
+    createLink.setAttribute("type","text/css")
+    createLink.setAttribute("href","https://fonts.googleapis.com/css?family="+fontName);
+    document.body.appendChild(createLink);
+    documentId.style.fontFamily=fontNameCSS+",serif";
+    return fontNameCSS+" successfully loaded";
+  }
+}
